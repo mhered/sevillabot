a48c007e19a3b50c6acc21ff0ad46869dc607294
Manuel Heredia <manolo.heredia@gmail.com>
2024-04-15 01:07:00 +0200

    Fix window title for activate_bot.bash command

--- commands/activate_bot.bash
+++ commands/activate_bot.bash
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Open a new terminal and spawn the robot
-gnome-terminal --title="Image Viewer" -- bash -c "ssh -t mhered@sevillabot 'cd ~/robot_ws; source install/setup.bash; ros2 launch sevillabot launch_robot.launch.py';
+gnome-terminal --title="sevillabot" -- bash -c "ssh -t mhered@sevillabot 'cd ~/robot_ws; source install/setup.bash; ros2 launch sevillabot launch_robot.launch.py';
 exec bash"
